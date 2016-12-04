7d2402e99d3f62d6295c4d1a1d1fe1f46c19f2b5
jvegh <Vegh.Janos@gmail.com>
2016-12-04 19:48:29 +0100

    As received from EMPAthY86

---
+++ cmake/Docs.cmake
@@ -0,0 +1,5 @@
+# This file describes the directory structure of the simEMPA project
+# Uses ideas and code from:
+# http://www.kaizou.org/2014/11/typical-cmake-project/
+#
+# The main directory contains the main 
