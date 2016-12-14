53c6a58608af1337c2419b20850a05a7d4c1e3b3
Janos Vegh <Vegh.Janos@gmail.com>
2016-12-14 21:42:27 +0100

    removed debugging lines

--- main/ESsistMeMain.cpp
+++ main/ESsistMeMain.cpp
@@ -21,10 +21,6 @@ int main(int argc, char *argv[])
   Heading << GetAppName() << " GUI has started\n";
   string LogFileName = string("Log_")+string("TestFile");
   SetupSystemDirectories();
-  ESpectrumBase MySp;
-  cerr << MySp.X_Get(5) << endl;
-  cerr << MySp.X_Get_Kinetic(5) << endl;
-  cerr << MySp.X_Get_Binding(5) << endl;
 //  OpenSystemFiles(GetFileNameRoot(appname.str()).c_str(), LogFileName.c_str(), Heading.str());
   app.setApplicationName(appname.str().c_str());
   appname << "[*]";
