f2819ea87c661ea100f12918e1a20af16ed15cb1
Janos Vegh <Vegh.Janos@gmail.com>
2016-12-14 21:38:49 +0100

    New structure tested

--- test/EvalTest.cpp
+++ test/EvalTest.cpp
@@ -22,10 +22,20 @@ public:
  */
 TEST_F(EvalTest, Tools)
 {
-    ESpectrumBase SP;
+    double myints[]={10.,20.,30.,40.,50.,60.,70.};
+    std::vector<double> myvector (7);
+    std::copy ( myints, myints+7, myvector.begin() );
+    ESpectrumBase SP(&myvector);
     EXPECT_EQ(-1, SP.XEnergy_Get()); // After creating, the X energy is invalid
     EXPECT_FALSE(SP.XEnergy_Valid());
+    EXPECT_EQ(20,SP.Y_Get(1));
+    EXPECT_EQ(1,SP.X_Get(1));
+    EXPECT_EQ(1,SP.X_Get_Kinetic(1));
     SP.XEnergy_Set(1486.295);
+    EXPECT_EQ(20,SP.Y_Get(1));
+    EXPECT_EQ(1,SP.X_Get(1));
+    EXPECT_EQ(1,SP.X_Get_Kinetic(1));
+    EXPECT_EQ(1485.295,SP.X_Get_Binding(1));
     EXPECT_EQ(1486.295, SP.XEnergy_Get()); // After setting, the X energy is valid
     EXPECT_TRUE(SP.XEnergy_Valid());
 }
