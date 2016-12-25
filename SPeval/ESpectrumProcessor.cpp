23f27cc7962a67172ce8ad4ed10a1efbf5ba5dc8
jvegh <Vegh.Janos@gmail.com>
2016-12-25 10:23:50 +0100

    Started to develop

---
+++ SPeval/ESpectrumProcessor.cpp
@@ -0,0 +1,14 @@
+#include "ESpectrumProcessor.h"
+
+/*!
+ * \brief ESpectrumProcessor::ESpectrumProcessor
+ *    The spectrum processor centralizes the operations and data fields,
+    responsible for processing experimental spectra
+ */
+
+
+ESpectrumProcessor::ESpectrumProcessor(void)
+{
+
+}
+
---
+++ SPeval/include/ESpectrumProcessor.h
@@ -0,0 +1,11 @@
+#ifndef ESPECTRUMPROCESSOR_H
+#define ESPECTRUMPROCESSOR_H
+
+
+class ESpectrumProcessor
+{
+public:
+    ESpectrumProcessor();
+};
+
+#endif // ESPECTRUMPROCESSOR_H
