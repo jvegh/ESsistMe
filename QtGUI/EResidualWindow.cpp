36e26052b30485a6c1f059b0fcfb833c43069665
jvegh <Vegh.Janos@gmail.com>
2016-12-25 21:04:39 +0100

    Prepared for disyplaying residual histogram

--- QtGUI/EResidualWindow.cpp
+++ QtGUI/EResidualWindow.cpp
@@ -18,7 +18,16 @@ ESsistMe_ResidualWindow::~ESsistMe_ResidualWindow()
 {
  //   delete ui;
 }
+int NDist = 22; double Range = 1.5; double DRange = NDist/2./Range; double MRange = -Range;
+double Rounding = Range/NDist;
 
+QVector<double> RGraph(NDist+2,-0.0L); QVector<double> RGraphX(NDist+2);
+
+
+int MyIndex(double Val)
+{
+    return Val < MRange ? 0 : Val>Range ? NDist+1 : (Val-MRange)*DRange + .99999;
+}
 
 void ESsistMe_ResidualWindow::setupSimpleDemo(QCustomPlot *customPlot)
 {
@@ -29,4 +38,34 @@ void ESsistMe_ResidualWindow::setupSimpleDemo(QCustomPlot *customPlot)
   customPlot->graph(0)->setBrush(QBrush(QColor(0, 0, 255, 20))); // first graph will be filled with translucent blue
   customPlot->graph(0)->setData(X1,  YResidual);
   customPlot->graph(0)->rescaleAxes();
+  customPlot->yAxis->setRange(MRange-Rounding, Range+Rounding);
+  customPlot->yAxis2->setRange(MRange-Rounding, Range+Rounding);
+  // Here we have two scales
+  // for the two graphs
+//  customPlot->graph(0)->rescaleAxes();
+  double MyMax = 0;
+  double CellWidth = 2*Range/NDist;
+    for(int i = 0; i<NDist+2; i++)
+    {
+        RGraphX[i] = MRange + (i-1)*CellWidth;
+        if(RGraphX[i] > MyMax) MyMax = RGraphX[i];
+    }
+    MyMax = 2*MyMax;
+  for (int i=0; i<50; ++i)
+  {
+
+    int j =  MyIndex(YResidual[i]);
+    RGraph[j] += 500.0L;
+  }
+
+  customPlot->addGraph(yAxis,xAxis);
+  customPlot->graph(1)->setLineStyle(QCPGraph::lsLine);
+  customPlot->graph(1)->setPen(QPen(Qt::red));
+  customPlot->graph(1)->setBrush(QBrush(QColor(255, 0, 0, 20))); // first graph will be filled with translucent blue
+  customPlot->graph(1)->setData(  RGraphX, RGraph);
+  customPlot->xAxis2->setVisible(true);
+  customPlot->yAxis2->setVisible(true);
+  customPlot->yAxis2->setRange(0, MyMax);
+
+
 }
